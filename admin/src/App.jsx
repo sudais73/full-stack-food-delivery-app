3e177e112f73561920e2214a3f69bfa2c4921f6f
sudais73 <sudaisman193@gmail.com>
2025-07-13 22:56:28 +0300

    Update App.jsx add backend url to admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders';
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-   const url = 'http://localhost:4000'
+   const url = 'https://food-de-backend-2.onrender.com'
   return (
     <div>
       <ToastContainer/>
